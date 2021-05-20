300cb0a570c2ff9fa5e02a367a4d851529b2661c
ko25252929 <76517744+ko25252929@users.noreply.github.com>
2021-05-20 17:28:49 +0900

    画像投稿機能実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,5 @@
 class TweetsController < ApplicationController
+
   def index
     @tweets = Tweet.all
   end
@@ -6,15 +7,20 @@ class TweetsController < ApplicationController
   def new
     @tweet = Tweet.new
   end
-  
+
   def create
     Tweet.create(tweet_params)
   end
 
-   
+  
+  def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy
+  end
+
   private
   def tweet_params
-    params.require(:tweet).permit(:name, :text)
+    params.require(:tweet).permit(:name, :text, :image)
   end
 
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,8 @@
 class Tweet < ApplicationRecord
-  validates :text, presence: true
+  has_one_attached :image
+  
+  validates :text, presence: true, unless: :was_attached?
+  def was_attached?
+    self.image.attached?
+  end
 end
