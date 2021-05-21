8f63f6534a4618852261d4a7175aea40c372cc98
ko25252929 <76517744+ko25252929@users.noreply.github.com>
2021-05-21 12:01:01 +0900

    詳細機能実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,7 @@
 class TweetsController < ApplicationController
-
-  def index
+      before_action :set_tweet, only: [:edit, :show, ]
+  
+      def index
     @tweets = Tweet.all
   end
 
@@ -19,7 +20,6 @@ class TweetsController < ApplicationController
   end
 
   def edit
-    @tweet = Tweet.find(params[:id])
   end
 
   def update
@@ -27,9 +27,16 @@ class TweetsController < ApplicationController
     tweet.update(tweet_params)
   end
 
+  def show
+  end
+
+
   private
   def tweet_params
     params.require(:tweet).permit(:name, :text, :image)
   end
 
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
+  end
 end
